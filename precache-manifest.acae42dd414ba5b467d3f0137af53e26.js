09263e7bf9ef71040a9261485d5ed6bdb6092dbb
Labais <reinitto@gmail.com>
2019-07-29 10:34:23 +0200

    Updates

---
+++ precache-manifest.acae42dd414ba5b467d3f0137af53e26.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "823bed7816e57618dc0ae4dd0a72e5a2",
+    "url": "/quiz_builder/index.html"
+  },
+  {
+    "revision": "e0305f17f9554357f3d1",
+    "url": "/quiz_builder/static/js/2.2fbc3229.chunk.js"
+  },
+  {
+    "revision": "bbb2dcb6b2f75bdbf80c",
+    "url": "/quiz_builder/static/js/main.6d260516.chunk.js"
+  },
+  {
+    "revision": "33069fa00093f911d9ed",
+    "url": "/quiz_builder/static/js/runtime~main.a81d949b.js"
+  }
+]);
